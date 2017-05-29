9512856cdb479842bb23db3595b95acf38632a5a
dvikster <dvikster@gmail.com>
2017-05-29 18:13:35 +0300

    added spiral arhimeda

--- js/js.js
+++ js/js.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var pi = Math.PI;
 ctx.fillStyle='#2bbbad';
+
 var x, y, R, r, d, u;
 //u = 0;
 //r = 80; маленький круг радиуса r  с отверстием на расстоянии d от центра вращается в большом (радиуса R), называется гипотрохоидой.
@@ -30,10 +31,66 @@ document.getElementById('button').onclick= function(){
 function spiro(){
 	x = (R-r)*Math.cos(u)+d*Math.cos(u*(R-r)/r);
 	y = (R-r)*Math.sin(u)-d*Math.sin(u*(R-r)/r);
-	ctx.fillRect(2*x+250,2*y+250, 2,2);
+	ctx.fillRect(2*x+300,2*y+300, 2,2);
     u = u+ 0.02;
 	setTimeout(spiro, 10);
 }
 
 spiro();
+
+
+var canvas2 = document.getElementById('canvas2');
+var ctx2 = canvas2.getContext('2d');
+ctx2.fillStyle='#2bbbad';
+var x1, y1, r1, u1;
+var timer='';
+//u = 0;
+//r = 80; маленький круг радиуса r  с отверстием на расстоянии d от центра вращается в большом (радиуса R), называется гипотрохоидой.
+//R= 50;
+//d=70;
+function koord() {
+    ctx2.beginPath();
+    ctx2.strokeStyle='#ee6e73';
+    ctx2.lineWidth = '2';
+    ctx2.moveTo(0,300);//начало
+    ctx2.lineTo(600,300);
+    ctx2.stroke();
+    ctx2.beginPath();
+    ctx2.lineWidth = '2';
+    ctx2.moveTo(300,0);//начало
+    ctx2.lineTo(300,600);
+    ctx2.stroke();
+}
+koord();
+
+document.getElementById('arhimed').onclick= function(){
+    spiralArhimeda();
+};
+document.getElementById('stop').onclick= function(){
+    clearTimeout(timer);
+};
+document.getElementById('reset').onclick= function(){
+    clearTimeout(timer);
+    u1 = 0;
+    r1 = 10;
+    ctx2.clearRect(0,0,600,600);
+    koord();
+};
+u1 = 0;
+r1 = 10;
+function spiralArhimeda(){
+
+    if(r1<290) {
+        x1 = r1 * Math.cos(0.2 * r1),
+            y1 = r1 * Math.sin(0.2 * r1);
+        ctx2.fillRect(300 + x1, 300 + y1, 2, 2);
+        r1 = r1 + 0.1;
+        timer = setTimeout(spiralArhimeda, 5);
+    }
+    else{
+        clearTimeout(timer);
+    }
+}
+
+
 
